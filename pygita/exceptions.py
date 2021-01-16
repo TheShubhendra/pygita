2f39cb35c7909a41a94d366d95ea89b7ed8a768f
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-01-16 18:38:39 +0530

    Update exceptions

--- pygita/exceptions.py
+++ pygita/exceptions.py
@@ -1,33 +1,37 @@
 """Module of execption classes."""
 
 
-class PygitaExceptions(Exception):
+class PygitaException(Exception):
     """Base class for exceptions."""
     pass
 
+class ConnectionError(PygitaException):
+    """Unable to connect with server."""
+    pass
 
-class BadRequestError(PygitaExceptions):
+
+class BadRequestError(PygitaException):
     """Bad Request: The request was unacceptable
 due to wrong parameter(s)."""
     pass
 
 
-class UnauthorisedError(PygitaExceptions):
+class UnauthorisedError(PygitaException):
     """Unauthorized: Invalid access_token used."""
     pass
 
 
-class RequestFailedError(PygitaExceptions):
-    """API requested fail from server side."""
+class RequestFailedError(PygitaException):
+    """API request failed from server side."""
     pass
 
 
-class NotFoundError(PygitaExceptions):
+class NotFoundError(PygitaException):
     """Not Found: The chapter/verse number you are
 looking for could not be found."""
     pass
 
 
-class ServerError(PygitaExceptions):
-    """Server Error: Something went wrong on our end."""
+class ServerError(PygitaException):
+    """Server Error: Something went wrong on server end."""
     pass
