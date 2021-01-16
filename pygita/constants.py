b73c98b86f7f413c46f7095a2d4f89da19ac7af2
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-01-16 18:05:05 +0530

    Remove error messages

--- pygita/constants.py
+++ pygita/constants.py
@@ -10,13 +10,5 @@ VERSE_COUNT = {
              16: 23, 17: 28, 18: 77
 }
 
-ERROR_MESSAGE = {400: '''Bad Request: The request was unacceptable
-due to wrong parameter(s).''',
-                 401: 'Unauthorized: Invalid access_token used.',
-                 402: 'Request Failed.',
-                 404: '''Not Found: The chapter/verse number you are
-looking for could not be found.''',
-                 500: 'Server Error: Something went wrong on our end.',
-                 }
 
 TOKEN_VALIDITY = 5*60
