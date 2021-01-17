2ff5cb08f3e0fee4a920e636c55580ffa21a798e
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-01-17 09:53:47 +0530

    Add Verse class

---
+++ pygita/verse.py
@@ -0,0 +1,33 @@
+class Verse:
+
+    def __init__(
+        self,
+        client,
+        json_data,
+        ):
+        """
+        Constructs all the necessary attributes for the Verse object.
+
+        Parameters
+        ----------
+            json_data : json
+                data in json format
+            language : str
+                language hi/en
+        """
+
+        self.language = language
+        self.__json = json_data
+        self.text = json_data['text']
+        self.meaning = json_data['meaning']
+        self.transliteration = json_data['transliteration']
+        self.chapter_number = int(json_data['chapter_number'])
+        self.verse_number = int(json_data['verse_number'])
+        self.word_meanings = json_data['word_meanings']
+        self.client = client
+
+    def json(self):
+        return self.__json
+
+    def __str__(self):
+        return self.text
