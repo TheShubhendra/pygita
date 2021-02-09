181d11601be9a92348759981d2b61d3d382704f2
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-09 10:46:23 +0530

    Add unit test function

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -10,3 +10,8 @@ def test_wrong_crendetials():
     with pytest.raises(AuthorizationError) as e:
         assert client.get_verse(1, 1)
     assert str(e.value) == "Unable to get access_token."
+
+
+def test_base_exception(client):
+    with pytest.raises(Exception):
+        assert client.get_verse(900, 800)
